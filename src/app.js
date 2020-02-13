5067d1738a5f175cc2c56bb5ce73f1a96bc4b4c3
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-02-13 11:01:16 +0200

    directed polling to rest api, checked some process exit codes, added function to tap in to check process progress, fixed some bugs, Content-Type -> content-type

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import fs from 'fs';
 import axios from 'axios';
 import ProcessError from '@natlibfi/melinda-commons';
-import {mongoFactory, QUEUE_ITEM_STATE} from '@natlibfi/melinda-rest-api-commons';
+import {QUEUE_ITEM_STATE} from '@natlibfi/melinda-rest-api-commons';
 import {promisify} from 'util';
-import {REST_API_PASSWORD, REST_API_USERNAME, REST_API_URL, MONGO_URI, RECORD_LOAD_API_RESULT_FILE_PATH} from './config';
+import {REST_API_PASSWORD, REST_API_USERNAME, REST_API_URL} from './config';
 import ora from 'ora';
 
 const setTimeoutPromise = promisify(setTimeout);
@@ -11,135 +11,155 @@ const setTimeoutPromise = promisify(setTimeout);
 run();
 
 async function run() {
-	const mongoOperator = await mongoFactory(MONGO_URI);
-	let pollParams;
-	let currentQueueItemState;
-	let lastChangeTime;
-	const spinner = ora('Start bulk loader!').start();
+	let correlationId;
+	let currentQueueItemState = '';
+	let lastChangeTime = '';
+	let spinner;
+	if (process.argv[2] === '' && process.argv[3] !== null) {
+		spinner = ora('Start track status!').start();
+		correlationId = process.argv[3]
+		pollResult();
+	} else {
+		spinner = ora('Start bulk loader!').start();
 
-	const args = process.argv[2].split(',');
-	/* P_manage_18 arguments (23_3)
-    [
-        "p_active_library",     // 0
-        "p_input_file",         // 1
-        "p_reject_file",        // 2
-        "p_log_file",           // 3
-        "p_old_new",            // 4
-        "p_fix_type",           // 5
-        "p_check_references",   // 6
-        "p_update_f",           // 7
-        "p_update_type",        // 8
-        "p_update_mode",        // 9
-        "p_char_conv",          // 10
-        "p_merge_type",         // 11
-        "p_cataloger_in",       // 12
-        "p_cataloger_level_x",  // 13
-        "p_z07_priority_year",  // 14
-        "p_redirection_field"   // 15
-    ]
-    */
+		const args = process.argv[2].split(',');
+		/* P_manage_18 arguments (23_3)
+		[
+			"p_active_library",     // 0
+			"p_input_file",         // 1
+			"p_reject_file",        // 2
+			"p_log_file",           // 3
+			"p_old_new",            // 4
+			"p_fix_type",           // 5
+			"p_check_references",   // 6
+			"p_update_f",           // 7
+			"p_update_type",        // 8
+			"p_update_mode",        // 9
+			"p_char_conv",          // 10
+			"p_merge_type",         // 11
+			"p_cataloger_in",       // 12
+			"p_cataloger_level_x",  // 13
+			"p_z07_priority_year",  // 14
+			"p_redirection_field"   // 15
+		]
+		*/
 
-	// For the test
-	// args[1] = '/file/location/here/test.seq';
-	// args[3] = 'test.syslog';
-	// args[12] = 'test000';
+		// For the test
+		// args[1] = '/file/location/here/test.seq';
+		// args[3] = 'test.syslog';
+		// args[12] = 'test000';
 
-	try {
-		const params = {
-			library: args[0],
-			inputFile: args[1],
-			rejectedFile: args[2],
-			resultFile: args[3],
-			method: args[4],
-			fixRoutine: args[5],
-			indexing: args[7],
-			updateAction: args[8],
-			mode: args[9],
-			charConversion: args[10],
-			mergeRoutine: args[11],
-			cataloger: args[12], // Replace with REST_API_USERNAME? To avoid miss usage
-			catalogerLevel: args[13],
-			indexingPriority: args[14]
-		};
-		spinner.start('Loaded parmas');
-		// Console.log(params);
-
-		// Check if the file is readable.
-		spinner.start('Checking file');
 		try {
-			fs.accessSync(params.inputFile, fs.constants.R_OK);
-			// Console.log(`${file} is readable`);
+			const params = {
+				library: args[0],
+				inputFile: args[1],
+				rejectedFile: args[2],
+				resultFile: args[3],
+				method: args[4],
+				fixRoutine: args[5],
+				indexing: args[7],
+				updateAction: args[8],
+				mode: args[9],
+				charConversion: args[10],
+				mergeRoutine: args[11],
+				cataloger: args[12], // Replace with REST_API_USERNAME? To avoid miss usage
+				catalogerLevel: args[13],
+				indexingPriority: args[14]
+			};
+			spinner.start('Loaded parmas');
+			// Console.log(params);
+
+			// Check if the file is readable.
+			spinner.start('Checking file');
+			try {
+				fs.accessSync(params.inputFile, fs.constants.R_OK);
+				// Console.log(`${file} is readable`);
+			} catch (err) {
+				console.log(params.inputFile)
+				console.log(err);
+				throw new ProcessError(404, 'Inputfile not found');
+			}
+
+			const stream = fs.createReadStream(params.inputFile);
+
+			spinner.start('Uploading file to queue');
+			const response = await axios({
+				method: 'post',
+				baseURL: REST_API_URL,
+				url: (params.method === 'OLD') ? 'bulk/update' : 'bulk/create',
+				headers: {"content-type": 'application/alephseq'},
+				params,
+				data: stream,
+				responseType: 'json',
+				auth: {
+					username: REST_API_USERNAME,
+					password: REST_API_PASSWORD
+				},
+				maxContentLength: 13000000
+			});
+
+			spinner.succeed('Files has been set to queue');
+			console.log(response.data);
+
+			correlationId = response.data.correlationId,
+
+				console.log('Waiting for status updates');
+
+			currentQueueItemState = response.data.queueItemState;
+			lastChangeTime = response.data.modificationTime;
+			await pollResult();
 		} catch (err) {
-			console.log(params.inputFile)
-			console.log(err);
-			throw new ProcessError(404, 'Inputfile not found');
+			console.log('error', err);
+			process.exit(5);
 		}
-
-		const stream = fs.createReadStream(params.inputFile);
-
-		spinner.start('Uploading file to queue');
-		const response = await axios({
-			method: 'post',
-			baseURL: REST_API_URL,
-			url: (params.method === 'OLD') ? 'bulk/update' : 'bulk/create',
-			headers: {'Content-Type': 'application/alephseq'},
-			params,
-			data: stream,
-			auth: {
-				username: REST_API_USERNAME,
-				password: REST_API_PASSWORD
-			},
-			maxContentLength: 13000000
-		});
-
-		spinner.succeed('Files has been set to queue');
-		console.log(response.data);
-
-		// Backup location for correlationId if this software shutsdown
-		fs.writeFileSync(RECORD_LOAD_API_RESULT_FILE_PATH + args[3] + '.correlationId', response.data.correlationId);
-
-		pollParams = {
-			correlationId: response.data.correlationId,
-			cataloger: response.data.cataloger,
-			operation: response.data.operation
-		};
-
-		console.log('Waiting for status updates');
-
-		currentQueueItemState = response.data.queueItemState;
-		lastChangeTime = response.data.modificationTime;
-		await pollResult();
-	} catch (err) {
-		console.log('error', err);
-		process.exit(0);
 	}
 
-	async function pollResult(wait) {
-		if (wait) {
-			await setTimeoutPromise(200);
-		}
+	async function pollResult(wait = false) {
+		try {
+			if (wait) {
+				await setTimeoutPromise(1000);
+			}
 
-		const result = await mongoOperator.query(pollParams);
-		if (result[0].queueItemState !== currentQueueItemState || result[0].modificationTime !== lastChangeTime) {
-			currentQueueItemState = result[0].queueItemState;
-			lastChangeTime = result[0].modificationTime;
-			spinner.start(`${currentQueueItemState} modification time: ${lastChangeTime} , Ids handled: ${result[0].handledIds.length}`);
-		}
+			const response = await axios({
+				method: 'get',
+				baseURL: REST_API_URL,
+				url: `bulk/?id=${correlationId}`,
+				headers: {"content-type": 'application/alephseq'},
+				responseType: 'json',
+				auth: {
+					username: REST_API_USERNAME,
+					password: REST_API_PASSWORD
+				}
+			});
 
-		if (result[0].queueItemState.startsWith(QUEUE_ITEM_STATE.ERROR)) {
-			spinner.fail('Request has failed');
-			console.log(result[0]);
-			process.exit(0);
-		}
+			if (response.data === []) {
+				throw new ProcessError(404, `Queue item ${correlationId} not found!`);
+			}
 
-		if (result[0].queueItemState === QUEUE_ITEM_STATE.DONE) {
-			spinner.succeed('Request has been handled');
-			console.log(result[0]);
-			fs.writeFileSync(RECORD_LOAD_API_RESULT_FILE_PATH + args[3], result[0].handledIds.map(id => id + args[0]).join('\n'));
-			fs.unlinkSync(RECORD_LOAD_API_RESULT_FILE_PATH + args[3] + '.correlationId');
-			process.exit(0);
-		} else {
-			pollResult(true);
+			const result = response.data[0];
+
+			if (result.queueItemState !== currentQueueItemState || result.modificationTime !== lastChangeTime) {
+				currentQueueItemState = result.queueItemState;
+				lastChangeTime = result.modificationTime;
+				spinner.start(`${currentQueueItemState} modification time: ${lastChangeTime} , Ids handled: ${result.handledIds.length}`);
+			}
+
+			if (result.queueItemState.startsWith(QUEUE_ITEM_STATE.ERROR)) {
+				spinner.fail('Request has failed');
+				console.log(result);
+				process.exit(0);
+			}
+
+			if (result.queueItemState === QUEUE_ITEM_STATE.DONE) {
+				spinner.succeed('Request has been handled');
+				console.log(result);
+				process.exit(0);
+			} else {
+				pollResult(true);
+			}
+		} catch (error) {
+			console.log('error', error);
+			process.exit(5);
 		}
 	}
 }
--- src/config.js
+++ src/config.js
@@ -2,10 +2,6 @@ import {Utils} from '@natlibfi/melinda-commons';
 
 const {readEnvironmentVariable} = Utils;
 
-export const MONGO_URI = readEnvironmentVariable('MONGO_URI', {defaultValue: 'mongodb://localhost:27017/db'});
-
 export const REST_API_URL = readEnvironmentVariable('REST_API_URL');
 export const REST_API_USERNAME = readEnvironmentVariable('REST_API_USERNAME');
 export const REST_API_PASSWORD = readEnvironmentVariable('REST_API_PASSWORD');
-
-export const RECORD_LOAD_API_RESULT_FILE_PATH = readEnvironmentVariable('RECORD_LOAD_API_RESULT_FILE_PATH');
