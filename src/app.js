964873df130dbeb2ce64a51b3f1925c911a25319
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-04-21 10:27:47 +0300

    implenting new logging and pr fixes

--- src/app.js
+++ src/app.js
@@ -1,115 +1,75 @@
 import fs from 'fs';
 import axios from 'axios';
-import {Error} from '@natlibfi/melinda-commons';
-import {QUEUE_ITEM_STATE} from '@natlibfi/melinda-rest-api-commons';
+import {Error as ApiError, Utils} from '@natlibfi/melinda-commons';
+import {logError, QUEUE_ITEM_STATE} from '@natlibfi/melinda-rest-api-commons';
 import {promisify} from 'util';
 import {REST_API_PASSWORD, REST_API_USERNAME, REST_API_URL} from './config';
-import ora from 'ora';
 
 const setTimeoutPromise = promisify(setTimeout);
 
-run();
+export default function () {
+  const {createLogger} = Utils;
+  const logger = createLogger(); // eslint-disable-line no-unused-vars
 
-async function run() {
-  let correlationId; // eslint-disable-line functional/no-let
-  let currentQueueItemState = ''; // eslint-disable-line functional/no-let
-  let lastChangeTime = ''; // eslint-disable-line functional/no-let
-  let spinner; // eslint-disable-line functional/no-let
+  return {newProcess, pollResult};
 
-  console.log('Melinda record load rest api bridge'); // eslint-disable-line no-console
-  if (process.argv[2] === '' && process.argv[3] !== null) {
-    spinner = ora('Start track status!').start();
-    [, , ,correlationId] = process.argv;
-    return pollResult();
-  }
-
-  spinner = ora('Start bulk loader!').start();
-
-  const args = process.argv[2].split(',');
-
-  /* P_manage_18 arguments (23_3)
-  [
-    "p_active_library",     // 0
-    "p_input_file",         // 1
-    "p_reject_file",        // 2
-    "p_log_file",           // 3
-    "p_old_new",            // 4
-    "p_fix_type",           // 5
-    "p_check_references",   // 6
-    "p_update_f",           // 7
-    "p_update_type",        // 8
-    "p_update_mode",        // 9
-    "p_char_conv",          // 10
-    "p_merge_type",         // 11
-    "p_cataloger_in",       // 12
-    "p_cataloger_level_x",  // 13
-    "p_z07_priority_year",  // 14
-    "p_redirection_field"   // 15
-  ]
-  */
-
-  try {
-    // Turn params to camelCase and ditching parametters after 5th
-    const params = {
-      pActiveLibrary: args[0],
-      pInputFile: args[1],
-      pRejectFile: args[2],
-      pLogFile: args[3],
-      pOldNew: args[4]
-    };
-    spinner.start('Loaded parmas');
-    // Console.log(params); // eslint-disable-line no-console
-
-    // Check if the file is readable.
-    spinner.start('Checking file');
+  async function newProcess(params) {
     try {
-      fs.accessSync(params.pInputFile, fs.constants.R_OK);
-      // Console.log(`${file} is readable`); // eslint-disable-line no-console
+      logger.log('verbose', 'Settings loaded');
+      logger.log('debug', `Settings:\n${JSON.stringify(params)}`);
+
+      const stream = readFiletoStream(params.pInputFile);
+
+      logger.log('verbose', 'Uploading file to queue');
+      const response = await axios({
+        method: 'post',
+        baseURL: REST_API_URL,
+        url: params.pOldNew === 'OLD' ? 'bulk/update' : 'bulk/create',
+        headers: {'content-type': 'application/alephseq'},
+        params,
+        data: stream,
+        responseType: 'json',
+        auth: {
+          username: REST_API_USERNAME,
+          password: REST_API_PASSWORD
+        },
+        maxContentLength: 13000000
+      });
+
+      logger.log('http', `Response status: ${response.status}`);
+
+      logger.log('verbose', 'Files has been set to queue');
+      logger.log('debug', `Response:\n${JSON.stringify(response.data.value)}`);
+
+      const result = response.data.value;
+      logger.log('info', `Waiting for status updates to ${result.correlationId}`);
+
+      logger.log('verbose', `${result.queueItemState || 'Waiting...'} modification time: ${result.modificationTime} , Ids handled: ${result.handledIds.length}`);
+      return pollResult(result.correlationId);
     } catch (err) {
-      console.log(params.pInputFile); // eslint-disable-line no-console
-      console.log(err); // eslint-disable-line no-console
-      throw new Error(404, 'Inputfile not found');
+      logError(err);
+      return process.exit(1); // eslint-disable-line no-process-exit
     }
 
-    const stream = fs.createReadStream(params.pInputFile);
+    function readFiletoStream(pInputFile) {
+      // Check if the file is readable.
+      logger.log('verbose', 'Checking file');
+      try {
+        fs.accessSync(pInputFile, fs.constants.R_OK);
+        logger.log('debug', `${pInputFile} is readable`);
+      } catch (err) {
+        throw new ApiError(404, `Inputfile not found or not accessable at ${pInputFile}`);
+      }
 
-    spinner.start('Uploading file to queue');
-    const response = await axios({
-      method: 'post',
-      baseURL: REST_API_URL,
-      url: params.pOldNew === 'OLD' ? 'bulk/update' : 'bulk/create',
-      headers: {'content-type': 'application/alephseq'},
-      params,
-      data: stream,
-      responseType: 'json',
-      auth: {
-        username: REST_API_USERNAME,
-        password: REST_API_PASSWORD
-      },
-      maxContentLength: 13000000
-    });
-
-    spinner.succeed('Files has been set to queue');
-    console.log(response.data.value); // eslint-disable-line no-console
-
-    correlationId = response.data.value.correlationId; // eslint-disable-line prefer-destructuring
-
-    console.log('Waiting for status updates'); // eslint-disable-line no-console
-
-    currentQueueItemState = response.data.value.queueItemState;
-    lastChangeTime = response.data.value.modificationTime;
-    spinner.start(`${currentQueueItemState} modification time: ${lastChangeTime} , Ids handled: ${response.data.value.handledIds.length}`);
-    await pollResult();
-  } catch (err) {
-    console.log('error', err); // eslint-disable-line no-console
-    return process.exit(1); // eslint-disable-line no-process-exit
+      return fs.createReadStream(params.pInputFile);
+    }
   }
 
-  async function pollResult(wait = false) {
+  async function pollResult(correlationId, modificationTime, wait = false) {
     try {
       if (wait) {
         await setTimeoutPromise(3000);
-        return pollResult();
+        return pollResult(correlationId, modificationTime);
       }
 
       const response = await axios({
@@ -125,34 +85,28 @@ async function run() {
       });
 
       if (response.data === []) { // eslint-disable-line functional/no-conditional-statement
-        throw new Error(404, `Queue item ${correlationId} not found!`);
+        throw new ApiError(404, `Queue item ${correlationId} not found!`);
       }
 
       const [result] = response.data;
 
-      if (result.queueItemState !== currentQueueItemState || result.modificationTime !== lastChangeTime) {
-        currentQueueItemState = result.queueItemState || 'Waiting...';
-        lastChangeTime = result.modificationTime;
-        spinner.start(`${currentQueueItemState} modification time: ${lastChangeTime} , Ids handled: ${result.handledIds.length}`);
-
-        return pollResult(true);
-      }
-
-      if (result.queueItemState === QUEUE_ITEM_STATE.ERROR) {
-        spinner.fail('Request has failed');
-        console.log(result); // eslint-disable-line no-console
-        return process.exit(0); // eslint-disable-line no-process-exit
+      if (result.queueItemState === QUEUE_ITEM_STATE.ERROR) { // eslint-disable-line functional/no-conditional-statement
+        throw new ApiError(500, `Process has failed ${JSON.stringify(result)}`);
       }
 
       if (result.queueItemState === QUEUE_ITEM_STATE.DONE) {
-        spinner.succeed('Request has been handled');
-        console.log(result); // eslint-disable-line no-console
+        logger.log('info', `Request has been handled:\n${JSON.stringify(result)}`);
         return process.exit(0); // eslint-disable-line no-process-exit
       }
 
-      pollResult(true);
+      if (modificationTime === result.modificationTime) {
+        return pollResult(correlationId, modificationTime, true);
+      }
+
+      logger.log('verbose', `${result.queueItemState || 'Waiting...'} modification time: ${result.modificationTime} , Ids handled: ${result.handledIds.length}`);
+      return pollResult(correlationId, result.modificationTime, true);
     } catch (error) {
-      console.log('error', error); // eslint-disable-line no-console
+      logError(error);
       return process.exit(1); // eslint-disable-line no-process-exit
     }
   }
---
+++ src/index.js
@@ -0,0 +1,122 @@
+/**
+*
+* @licstart  The following is the entire license notice for the JavaScript code in this file.
+*
+* Melinda-record-load-pi-bridge
+*
+* Copyright (C) 2018-2019 University Of Helsinki (The National Library Of Finland)
+*
+* This file is part of melinda-rest-api microservice
+*
+* melinda-rest-api microservice is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as
+* published by the Free Software Foundation, either version 3 of the
+* License, or (at your option) any later version.
+*
+* melinda-rest-api microservice is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+* @licend  The above is the entire license notice
+* for the JavaScript code in this file.
+*
+*/
+
+import {Error as ApiError, Utils} from '@natlibfi/melinda-commons';
+import * as config from './config';
+import startApp from './app';
+import {logError} from '@natlibfi/melinda-rest-api-commons';
+
+run();
+
+async function run() {
+  const {createLogger} = Utils;
+  const logger = createLogger(); // eslint-disable-line no-unused-vars
+  const {handleInterrupt} = Utils;
+  const rawPArgs = process.argv[2] || '';
+  const correlationId = process.argv[3] || null;
+
+  registerInterruptionHandlers();
+
+  const server = await startApp({...config}); // eslint-disable-line prefer-const
+
+  logger.log('info', 'Melinda record load rest api bridge'); // eslint-disable-line no-console
+  if (rawPArgs === '' && correlationId !== null) {
+    logger.log('info', `Start track process status of ${correlationId}!`);
+    return server.pollResult(correlationId);
+  }
+
+  if (rawPArgs !== '') {
+    logger.log('info', 'Start new bulk loader process!');
+    const pArgs = rawPArgs.split(',');
+
+    /* P_manage_18 arguments (23_3)
+      [
+      "p_active_library",     // 0
+      "p_input_file",         // 1
+      "p_reject_file",        // 2
+      "p_log_file",           // 3
+      "p_old_new",            // 4
+      "p_fix_type",           // 5
+      "p_check_references",   // 6
+      "p_update_f",           // 7
+      "p_update_type",        // 8
+      "p_update_mode",        // 9
+      "p_char_conv",          // 10
+      "p_merge_type",         // 11
+      "p_cataloger_in",       // 12
+      "p_cataloger_level_x",  // 13
+      "p_z07_priority_year",  // 14
+      "p_redirection_field"   // 15
+      ]
+    */
+    if (pArgs[0] && pArgs[1] && pArgs[2] && pArgs[3] && pArgs[4]) {
+      // Turn params to camelCase and ditching parametters after 5th
+      const params = {
+        pActiveLibrary: pArgs[0],
+        pInputFile: pArgs[1],
+        pRejectFile: pArgs[2],
+        pLogFile: pArgs[3],
+        pOldNew: pArgs[4]
+      };
+
+      return server.newProcess(params);
+    }
+
+    throw new ApiError(400, 'Bad arguments')
+  }
+
+  logger.log('info', 'To run new process args 2 "p_active_library,p_input_file,p_reject_file,p_log_file,p_old_new"');
+  logger.log('info', 'To track runnig process args 2 "" & args 3 <process id>');
+
+  function registerInterruptionHandlers() {
+    process
+      .on('SIGTERM', handleSignal)
+      .on('SIGINT', handleInterrupt)
+      .on('uncaughtException', ({stack}) => {
+        handleTermination({code: 1, message: stack});
+      })
+      .on('unhandledRejection', ({stack}) => {
+        handleTermination({code: 1, message: stack});
+      });
+
+    function handleTermination({code = 0, message = false}) {
+      logMessage(message);
+      process.exit(code); // eslint-disable-line no-process-exit
+    }
+
+    function handleSignal(signal) {
+      handleTermination({code: 1, message: `Received ${signal}`});
+    }
+
+    function logMessage(message) {
+      if (message) {
+        return logError(message);
+      }
+    }
+  }
+}
