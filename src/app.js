c519edc7e7d7f7872f9f179d864c340979c0a779
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-05-12 12:29:53 +0300

    implement new commons api client

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import fs from 'fs';
-import fetch from 'node-fetch';
-import {Error as ApiError, Utils} from '@natlibfi/melinda-commons';
+import {Error as ApiError, Utils, createApiClient} from '@natlibfi/melinda-commons';
 import {logError, QUEUE_ITEM_STATE} from '@natlibfi/melinda-rest-api-commons';
 import httpStatus from 'http-status';
 import {promisify} from 'util';
@@ -10,6 +9,7 @@ const setTimeoutPromise = promisify(setTimeout);
 export default function ({restApiPassword, restApiUsername, restApiUrl}, handleUnexpectedAppError) {
   const {createLogger} = Utils;
   const logger = createLogger();
+  const client = createApiClient({restApiPassword, restApiUsername, restApiUrl});
 
   return {newProcess, pollResult};
 
@@ -18,34 +18,14 @@ export default function ({restApiPassword, restApiUsername, restApiUrl}, handleU
       logger.log('verbose', 'Settings loaded');
       logger.log('debug', `Settings:\n${JSON.stringify(params)}`);
 
-      const query = new URLSearchParams(params);
-      const url = new URL(`${restApiUrl}bulk/?${query}`);
-
       logger.log('verbose', 'Uploading file to queue');
-      logger.log('debug', url.toString());
-      const response = await fetch(url, {
-        method: 'post',
-        headers: {
-          'content-type': 'application/alephseq',
-          'Authorization': `Basic ${Buffer.from(`${restApiUsername}:${restApiPassword}`).toString('base64')}`,
-          'Accept': 'application/json'
-        },
-        body: readFiletoStream(params.pInputFile)
-      });
+      const result = await client.postBulk({params, contentType: 'application/alephseq', body: readFiletoStream(params.pInputFile)});
+      logger.log('verbose', 'Files has been set to queue');
+      logger.log('debug', `Response:\n${JSON.stringify(result.value)}`);
+      logger.log('info', `Waiting for status updates to ${result.value.correlationId}`);
+      logger.log('verbose', `${result.value.queueItemState || 'Waiting...'} modification time: ${result.value.modificationTime} , Ids handled: ${result.value.handledIds.length || 0}`);
 
-      logger.log('http', `Response status: ${response.status}`);
-
-      if (response.status === httpStatus.OK) {
-        const result = await response.json();
-        logger.log('verbose', 'Files has been set to queue');
-        logger.log('debug', `Response:\n${JSON.stringify(result.value)}`);
-        logger.log('info', `Waiting for status updates to ${result.value.correlationId}`);
-        logger.log('verbose', `${result.value.queueItemState || 'Waiting...'} modification time: ${result.value.modificationTime} , Ids handled: ${result.value.handledIds.length || 0}`);
-
-        return pollResult(result.value.correlationId);
-      }
-
-      throw new ApiError(response.status, await response.text());
+      return pollResult(result.value.correlationId);
     } catch (err) {
       logError(err);
       return handleUnexpectedAppError('Unexpected error in newProcess');
@@ -65,52 +45,33 @@ export default function ({restApiPassword, restApiUsername, restApiUrl}, handleU
     }
   }
 
-  async function pollResult(correlationId, modificationTime, wait = false) {
+  async function pollResult(correlationId, modificationTime = null, wait = false) {
     try {
       if (wait) {
         await setTimeoutPromise(3000);
         return pollResult(correlationId, modificationTime);
       }
-      const query = new URLSearchParams({id: correlationId});
-      const url = new URL(`${restApiUrl}bulk/?${query}`);
 
-      logger.log('silly', url.toString());
+      const result = await client.getMetadata({id: correlationId});
 
-      const response = await fetch(url, {
-        method: 'get',
-        headers: {
-          'Content-Type': 'application/alephseq',
-          'Authorization': `Basic ${Buffer.from(`${restApiUsername}:${restApiPassword}`).toString('base64')}`,
-          'Accept': 'application/json'
-        }
-      });
-
-      logger.log('silly', `Response status: ${response.status}`);
-
-      if (response.status === httpStatus.OK) {
-        const [result] = await response.json();
-
-        if (result === undefined) { // eslint-disable-line functional/no-conditional-statement
-          throw new ApiError(404, `Queue item ${correlationId} not found!`);
-        }
-
-        if (result.queueItemState === QUEUE_ITEM_STATE.ERROR) { // eslint-disable-line functional/no-conditional-statement
-          throw new ApiError(500, `Process has failed ${JSON.stringify(result)}`);
-        }
-
-        if (result.queueItemState === QUEUE_ITEM_STATE.DONE) {
-          return logger.log('info', `Request has been handled:\n${JSON.stringify(result)}`);
-        }
-
-        if (modificationTime === result.modificationTime) {
-          return pollResult(correlationId, modificationTime, true);
-        }
-
-        logger.log('verbose', `${result.queueItemState || 'Waiting...'} modification time: ${result.modificationTime} , Ids handled: ${result.handledIds.length}`);
-        return pollResult(correlationId, result.modificationTime, true);
+      if (result === undefined) { // eslint-disable-line functional/no-conditional-statement
+        throw new ApiError(404, `Queue item ${correlationId} not found!`);
       }
 
-      throw new ApiError(response.status, await response.text());
+      if (result.queueItemState === QUEUE_ITEM_STATE.ERROR) { // eslint-disable-line functional/no-conditional-statement
+        throw new ApiError(500, `Process has failed ${JSON.stringify(result)}`);
+      }
+
+      if (result.queueItemState === QUEUE_ITEM_STATE.DONE) {
+        return logger.log('info', `Request has been handled:\n${JSON.stringify(result)}`);
+      }
+
+      if (modificationTime === result.modificationTime || modificationTime === null) {
+        return pollResult(correlationId, modificationTime, true);
+      }
+
+      logger.log('verbose', `${result.queueItemState || 'Waiting...'} modification time: ${result.modificationTime} , Ids handled: ${result.handledIds.length}`);
+      return pollResult(correlationId, result.modificationTime, true);
     } catch (error) {
       return handleUnexpectedAppError(error.payload);
     }
