8910bed2c55cc81a6eed3fe6bd00f741f40d1413
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2020-06-26 17:15:21 +0300

    better handling on file params

--- src/utils.js
+++ src/utils.js
@@ -1,32 +1,31 @@
+import {defaultTempFilePath} from './config';
+
 export function handleArgsToParams(pArgs) {
   const params = {
     pActiveLibrary: pArgs[0],
-    pInputFile: pArgs[1],
+    pInputFile: handleFileParam(pArgs[0], pArgs[1], defaultTempFilePath),
+    pRejectFile: handleFileParam(pArgs[0], pArgs[2], defaultTempFilePath),
+    pLogFile: handleFileParam('alephe', pArgs[3], defaultTempFilePath),
     pOldNew: pArgs[4]
   };
 
-  const params2 = handlePRejectFile(params, pArgs[2]);
-  const params3 = handlePLogFile(params2, pArgs[3]);
-  return handlePCatalogerIn(params3, pArgs[12]);
-}
+  return handlePCatalogerIn(params, pArgs[12]);
 
-function handlePRejectFile(params, pArgs2) {
-  if (pArgs2 !== undefined && pArgs2 !== '') {
-    return {...params, pRejectFile: pArgs2};
-  }
-  return params;
-}
+  function handleFileParam(pActiveLibrary, inputParam, path) {
+    if (inputParam !== undefined && inputParam !== '') {
+      if (inputParam.indexOf(`${path}${pActiveLibrary.toLowerCase()}/scratch`) === -1) {
+        return `${path}${pActiveLibrary.toLowerCase()}/scratch/${inputParam}`;
+      }
+      return inputParam;
+    }
 
-function handlePLogFile(params, pArgs3) {
-  if (pArgs3 !== undefined && pArgs3 !== '') {
-    return {...params, pLogFile: pArgs3};
+    return false;
   }
-  return params;
-}
 
-function handlePCatalogerIn(params, pArgs12) {
-  if (pArgs12 !== undefined && pArgs12 !== '') {
-    return {...params, pCatalogerIn: pArgs12};
+  function handlePCatalogerIn(params, pArgs12) {
+    if (pArgs12 !== undefined && pArgs12 !== '') {
+      return {...params, pCatalogerIn: pArgs12};
+    }
+    return params;
   }
-  return params;
 }
